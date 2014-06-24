0926c79d0408ab1cdc06418a1a5fa0c4c0a4ed62
Niko Sams <ns@vivid-planet.com>
2014-06-24 13:26:10 +0200

    fixed prefix for deftjs sources

--- Kwf/DeftJS/Assets/JsDependency.php
+++ Kwf/DeftJS/Assets/JsDependency.php
@@ -1,6 +1,24 @@
 <?php
 class Kwf_DeftJS_Assets_JsDependency extends Kwf_Assets_Dependency_File_Js
 {
+    private $_fileNameCache;
+
+    public function getAbsoluteFileName()
+    {
+        if (isset($this->_fileNameCache)) return $this->_fileNameCache;
+
+        $pathType = $this->getType();
+        if ($pathType != 'deftjs') throw new Kwf_Exception("invalid path type: '$pathType'");
+
+        $paths = self::_getAllPaths();
+        $f = substr($this->_fileName, strpos($this->_fileName, '/'));
+        if (isset($paths[$pathType])) {
+            $f = $paths[$pathType]."/packages/deft/src/js/".$f;
+        }
+        $this->_fileNameCache = $f;
+        return $f;
+    }
+
     protected function _getRawContents($language)
     {
         $ret = parent::_getRawContents($language);
