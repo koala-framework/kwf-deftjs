d8f3cafd66f9d3902b802183b51a19063507eabe
Niko Sams <ns@vivid-planet.com>
2014-11-24 14:59:41 +0100

    rename Ext4 to Ext

--- Kwf/DeftJS/Assets/JsDependency.php
+++ Kwf/DeftJS/Assets/JsDependency.php
@@ -22,7 +22,6 @@ class Kwf_DeftJS_Assets_JsDependency extends Kwf_Assets_Dependency_File_Js
         unlink($inFile);
         unlink($outFile);
 
-        $ret = "(function(Ext) {".$ret."})(this.Ext4);";
         return $ret;
     }
 }
